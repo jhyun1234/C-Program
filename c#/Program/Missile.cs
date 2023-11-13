ac31bb2c70ee61473f9314d0cd70d38620e58e3d
jhyun1234 <anjyounl24@gmail.com>
2023-11-13 19:47:33 +0900

    2023.11.13 인터페이스와 추상클래스

---
+++ c#/Program/Missile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Missile : IItem
+    {
+        public void Use()
+        {
+            Console.WriteLine("Use Missile");
+        }
+    }
+}
