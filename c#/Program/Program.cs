9605bd6018d9c4b9e3a5d67a666a14c5d08153c0
jhyun1234 <anjyounl24@gmail.com>
2023-11-20 19:46:31 +0900

    2023.11.20 콜렉션

--- c#/Program/Program.cs
+++ c#/Program/Program.cs
@@ -1,43 +1,165 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections;
+using System.Runtime.InteropServices;
 using System.Threading.Channels;
 
 namespace Program
 {
-   
+   public class GameObject
+    {
+        private string name;
+        private string v;
+
+        public GameObject(string v)
+        {
+            this.v = v;
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+    }
     internal class Program
     {
    
         static void Main(string[] args)
         {
-            #region 일반화 프로그래밍
-            // 하나의 데이터가 특정 데이터 타입에만 종속되지 않고,
-            // 여러 데이터 타입을 가질 수 있는 기술에 중점을 두어
-            // 재사용성을 높일 수 있는 프로그래밍 방법이다.
+            #region 콜렉션
 
-            Deque<string> stringDeque = new Deque<string>();
-            Deque<int> intDeque = new Deque<int>();
-            stringDeque.Set("Box Collider");
-            intDeque.Set(1000);
+            //ArrayList
+            /*
+             * ArrayList arrayList = new ArrayList(); // 모든 타입을 다 받을 수 있다.
 
-            // C++ 템플릿은 컴파일이 한 번 일어나며, C# 일반화는 컴파일이 2번 일어난다.
+            arrayList.Add(100);
+            arrayList.Add("String");
+            arrayList.Add(115.5f);
+            arrayList.Add('A');
 
-            // C++ 템플릿은 사용하지 않으면 컴파일을 하지 않는다.
+            foreach(object element in arrayList)
+            {
+                Console.WriteLine(element);
+            }
 
-            // C# 일반화는 사용하지 않더라도 그에 관련된 정보를 저장하기 위한
-            // 메타 파일을 생성해야 하므로 컴파일이 진행된다.
+            //  0      1        2      3
+            // [10] [String] [115.5f] [A]
+
+            arrayList.Remove(100);
+            Console.WriteLine("  ");
+            Console.WriteLine("arrayList Count : "+arrayList.Count);
+
+
+            foreach (object element in arrayList)
+            {
+                Console.WriteLine(element);
+            }
+            */
+
+
+            // List 
+            /*
+            List<int> list = new List<int>(10);
+
+            list.Capacity = 10;
+            // [] [] [] [] [] [] [] [] [] []
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+
+            // list.RemoveAt(3); // 3번째 index가 삭제됨
+            // list.Reverse(); 순서를 반전시킴
+            // list.Sort(); // 작은 수(list 안에있는 원소) 부터 정렬함 
+            for(int i=0; i<list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            */
+            /*
+             List<string> list = new List<string>(5);
+             list.Capacity = 5;
+             list.Add("메이플스토리");
+             list.Add("로스트아크");
+             list.Add("서든어택");
+             list.Add("리그오브레전드");
+             list.Add("오버워치");
+
+
+             Random random = new Random();
+             int temp = list.Count;
+             for(int i=0; i<temp; i++)
+             {
+                 int rand = random.Next(0, list.Count);
+                 Console.WriteLine(list[rand]);
+                 list.RemoveAt(rand);
+             }
+             */
+
+            // LinkedList
+            /*
+            LinkedList<int> link = new LinkedList<int>();
+            link.AddFirst(10); 
+            link.AddFirst(20); // 20이 먼저 나오는 이유는 newNode가 첫번째 노드로 가 있기 때문
+            link.AddLast(30);
+            foreach(var item in link)
+            {
+                Console.WriteLine(item);
+            }
+            */
+
+            // Stack
+            /*
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+
+            stack.Pop();  // Pop: 가장 위에 있는 데이터를 삭제한다.
+                          // Peek : 가장 위에 있는 데이터를 반환한다.
+            Console.WriteLine("stack Peek : "+ stack.Peek());
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            */
+
+
+            // Queue
+            Queue<GameObject> queue = new Queue<GameObject>();
+            string[] objectName = new string[4] { "Cube", "Sphere", "Capsule", "Cylinder" };
+            queue.Enqueue(new GameObject("Cube")); // []
+            queue.Enqueue(new GameObject("Sphere")); // [] []
+            queue.Enqueue(new GameObject("Capsule")); // [] [] []
+            queue.Enqueue(new GameObject("Cylinder")); // [] [] [] []
+
+            int queueSize = queue.Count;
+            for(int i=0; i<queueSize; i++)
+            {
+                GameObject gameObject = queue.Dequeue();
+                gameObject.Name = objectName[i];
+                Console.WriteLine(gameObject.Name);
+            }
+
+            
+
+            
+
+
+            // Random Class
+            /*
+             * Random random = new Random();
+            int rand = random.Next(0, 10);
+
+            Console.WriteLine("rand : " + rand);
+            */
+
+
             #endregion
 
-            #region 범위 기반 반복문
 
-            //int[] buffer = new int[5];
-            //// Collection 
-            //// List , Dictionary ,LinkedList , Stack 
-
-            //foreach(int element in buffer)
-            //{
-            //    Console.WriteLine(element);
-            //}
-            #endregion
         }
     }
 }
