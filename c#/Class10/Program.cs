ebec5966737e31bb66ff1db66e5eb74f1e4349ad
KIM <anjyounl24@gmail.com>
2023-11-22 19:47:02 +0900

    2023.11.22 이진 탐색

---
+++ c#/Class10/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Class10
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            #region 등차수열
+
+
+            // 등차수열
+            /*
+            int commonDiffernt = 0; // 공차 
+            int firstTerm = 0; // 첫항
+
+            int size = 0;
+
+            // Console.ReadLine() : string 타입으로만 값을 반환한다.
+            // string name = Console.ReadLine();
+            // Console.WriteLine("name의 변수의 값 : " + name);
+            // Int32.Parse : int 타입으로 값을 반환한다.
+
+            Console.Write("첫 항의 값 입력 : ");
+            firstTerm = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.Write("공차의 값 입력 : ");
+            commonDiffernt = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.Write("n 의 값 입력 :");
+            size = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+
+            for (int i=0; i<size; i++)
+            {
+                Console.WriteLine(firstTerm + commonDiffernt *i);
+            }
+            */
+            #endregion
+
+            #region 등비수열
+            /*
+            int commonratio; // 공비
+            int firstTerm; // 첫항
+
+            int size;
+            Console.Write("첫 항의 값 입력 : ");
+            firstTerm = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.Write("공비의 값 입력 : ");
+            commonratio = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.Write("n 의 값 입력 :");
+            size = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            for (int i=1; i<size; i++)
+            {
+                Console.WriteLine(firstTerm);
+                firstTerm *= commonratio;
+            }
+            */
+
+            #endregion
+
+            #region 무명 형식
+            /*
+            // 이름이 존재하지 않는 객체 또는 변수를 의미 한다.
+
+            var data = new { Name = "Data", Value = 10 };
+            // 무명 형식의 경우 값을 읽기만 할 수 있다.
+            // data.Name=200; error
+
+            Console.WriteLine("data.Name : "+data.Name + " data.Value : "+data.Value);
+            */
+            #endregion
+        }
+    }
+}
--- c#/Program/Program.cs
+++ c#/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Runtime.InteropServices;
 using System.Threading.Channels;
+using System.Xml.Linq;
 
 namespace Program
 {
@@ -8,10 +9,31 @@ namespace Program
     internal class Program
     {
         
-
-        static void Main(string[] args)
+        void BinarySearch()
         {
             
+
+        }
+        static void Main(string[] args)
+        {
+            #region 이진 탐색
+
+            // 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘이다.
+
+            // 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정한다.
+
+            // 2. [pivot]의 값이 찾고자 하는 요소가 같다면 검색완료
+
+            // 3. [pivot]의 값이 찾는 값보다 크다면 left ~ pivot 사이를 검색한다.
+
+            // 4. [pivot]의 값이 찾는 값보다 작다면 pivot ~ right 사이를 검색한다.
+
+
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+
+
+            #endregion
 
         }
     }
