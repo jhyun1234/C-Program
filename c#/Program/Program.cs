717457dd72f239ee68798a557b1d732ff5972636
jhyun1234 <anjyounl24@gmail.com>
2023-11-23 19:45:46 +0900

    2023.11.23 퀵 정렬

--- c#/Program/Program.cs
+++ c#/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Runtime.InteropServices;
 using System.Threading.Channels;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace Program
@@ -8,31 +9,38 @@ namespace Program
    
     internal class Program
     {
-        
-        void BinarySearch()
+        static void QuickSort(int [] array,int right , int left)
         {
-            
+           // 1. left가 right보다 크거나 같아졌을 때
+           if(left >= right)
+           {
+                return;
+           }
 
         }
+        static void Swap(ref int x, ref int y)
+        {
+            int temporary = x;
+            x = y;
+            y = temporary;
+
+        }
+
         static void Main(string[] args)
         {
-            #region 이진 탐색
+            #region 퀵 정렬
+            // 기준점을 획득한 다음 해당 기준점을 기준으로
+            // 배열을 나누고 한 쪽에는 기준점보다 작은 항목들이
+            // 위치하고 다른 쪽에는 기준점 보다 큰 항목들이 위치한다.
 
-            // 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘이다.
+            // 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+            // 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘이다.
 
-            // 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정한다.
+            // 시간 복잡도 : O(log n)
+            int[] array = new int[10] { 3, 1, 2, 5, 4,6,7,8,11,10};
+            Array.Sort(array);
 
-            // 2. [pivot]의 값이 찾고자 하는 요소가 같다면 검색완료
-
-            // 3. [pivot]의 값이 찾는 값보다 크다면 left ~ pivot 사이를 검색한다.
-
-            // 4. [pivot]의 값이 찾는 값보다 작다면 pivot ~ right 사이를 검색한다.
-
-
-            int[] array = new int[5] { 1, 2, 3, 4, 5 };
-
-
-
+            BinarySearch(array, 12);
             #endregion
 
         }
